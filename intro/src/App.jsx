d9553faf2323042863a8fa69d29cac90b1e5ea9c
Muhammad-Zohaib-42 <muhammadzohaibranjha42@gmail.com>
2026-06-02 18:34:18 +0500

    learn how to add external styling and practice it

--- intro/src/App.jsx
+++ intro/src/App.jsx
@@ -1,7 +1,10 @@
+import "./index.css"
+
 const App = () => {
-  const styles = {color: "white", backgroundColor: "crimson", padding: "3rem"}
   return (
-    <h1 style={styles}>Hello World</h1>
+    <section>
+      <h1>Hello World</h1>
+    </section>
   )
 }
 
