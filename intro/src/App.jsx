fc41cc42633b4567a5ba538daefefed7b095e0b6
Muhammad-Zohaib-42 <muhammadzohaibranjha42@gmail.com>
2026-06-03 00:20:15 +0500

    develop a todo app using react state

--- intro/src/App.jsx
+++ intro/src/App.jsx
@@ -1,20 +1,45 @@
 import React, { useEffect, useState } from 'react'
 
 const App = () => {
-  const [name, setName] = useState(() => {
-    const savedName = localStorage.getItem("name")
-    return savedName ? JSON.parse(savedName) : ""
+  const [tasksData, setTasksData] = useState(() => {
+    const tasks = localStorage.getItem("tasks")
+    return tasks ? JSON.parse(tasks) : []
   })
+  const [task, setTask] = useState("")
 
   useEffect(() => {
-    localStorage.setItem("name", JSON.stringify(name))
-  }, [name])
+    localStorage.setItem("tasks", JSON.stringify(tasksData))
+  }, [tasksData])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    if (!task) alert("task field is required!")
+
+    const newTask = {
+      id: Date.now(),
+      task
+    }
+
+    setTasksData(prev => [...prev, newTask])
+  }
 
   return (
-    <div>
-      <h3>Your Name: {name}</h3>
-      <input type="text" value={name} onChange={e => setName(e.target.value)} placeholder='Enter name' />
-    </div>
+    <main>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='Enter task' value={task} onChange={e => setTask(e.target.value)} />
+        <button>Add Task</button>
+      </form>
+      <section>
+        {
+          tasksData.length < 1 ? <h1>No Tasks Yet</h1> : <ul>
+            {
+              tasksData.map(taskData => (<li key={taskData.id}>{taskData.task}</li>))
+            }
+          </ul>
+        }
+      </section>
+    </main>
   )
 }
 
