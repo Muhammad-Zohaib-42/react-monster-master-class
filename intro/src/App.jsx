e6e31d8a24cdcf36b785f39e649d267e9eea2860
Muhammad-Zohaib-42 <muhammadzohaibranjha42@gmail.com>
2026-06-02 16:44:16 +0500

    create a main content component and render between header and footer in app component

--- intro/src/App.jsx
+++ intro/src/App.jsx
@@ -1,9 +1,11 @@
 import Footer from "./components/Footer"
 import Header from "./components/Header"
+import MainContent from "./components/MainContent"
 
 const App = () => {
   return <>
     <Header />
+    <MainContent />
     <Footer />
   </>
 }
---
+++ intro/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MainContent = () => {
+  return (
+    <main>
+        <h2>Main Content</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam voluptatibus optio modi quibusdam possimus at impedit rerum delectus! Amet, libero cumque. Velit odio, tenetur debitis exercitationem nihil architecto doloremque atque!</p>
+    </main>
+  )
+}
+
+export default MainContent
