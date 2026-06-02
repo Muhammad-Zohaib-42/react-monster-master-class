823062ef7d4429d1f6eb675cd351bc45c9c6fdfe
Muhammad-Zohaib-42 <muhammadzohaibranjha42@gmail.com>
2026-06-03 02:11:32 +0500

    go deep in useeffect and learn how to fetch api data in useeffect hook

--- intro/src/App.jsx
+++ intro/src/App.jsx
@@ -1,18 +1,29 @@
 import React, { useEffect, useState } from 'react'
+import ProductCard from './components/ProductCard'
 
 const App = () => {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9])
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
-    console.log("useEffect runs")
-    document.title = `count: ${count}`
-  },[count])
+    async function fetchData() {
+      const response = await fetch('https://fakestoreapi.com/products')
+      const data = await response.json()
+      if (data && data.length) {
+        setIsLoading(false)
+        setData(data)
+      }
+    }
+    
+    fetchData()
+  }, [])
 
   return (
-    <div>
-      <p>{count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment Count</button>
-    </div>
+    <main style={{display:'grid',gridTemplateColumns: 'repeat(auto-fit, minmax(300px,1fr))', gap: '.5rem'}}>
+      {
+        data.map(obj => <ProductCard isLoading={isLoading} product={obj} />)
+      }
+    </main>
   )
 }
 
