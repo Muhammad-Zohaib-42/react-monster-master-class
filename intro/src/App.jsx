64def79f832b13434593a8961a5a123256a5571a
Muhammad-Zohaib-42 <muhammadzohaibranjha42@gmail.com>
2026-06-03 01:51:40 +0500

    learn the useeffect hook and practice coding example of it and also about dependency array

--- intro/src/App.jsx
+++ intro/src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-import Portal from './components/Portal'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    console.log("useEffect runs")
+    document.title = `count: ${count}`
+  },[count])
+
   return (
-    <Portal />
+    <div>
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment Count</button>
+    </div>
   )
 }
 
