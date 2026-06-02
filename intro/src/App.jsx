2a36da721fc9b45fea1f6081c564751091d00364
Muhammad-Zohaib-42 <muhammadzohaibranjha42@gmail.com>
2026-06-02 17:46:13 +0500

    lern about conditional rendering and practice it

--- intro/src/App.jsx
+++ intro/src/App.jsx
@@ -1,19 +1,10 @@
-import Card from "./components/Card"
-
 const App = () => {
+  const isLoggedIn = true
+  const isAdmin = true
+
   return <>
-    <Card>
-      <h2>Card 1</h2>
-      <p>this is card 1</p>
-    </Card>
-    <Card>
-      <h2>Card 2</h2>
-      <p>this is card 2</p>
-    </Card>
-    <Card>
-      <h2>Card 3</h2>
-      <p>this is card 3</p>
-    </Card>
+     {isLoggedIn && isAdmin && <h1>Welcom Admin</h1>}
+     {isLoggedIn && !isAdmin && <h1>Welcome user</h1>} 
   </>
 }
 
