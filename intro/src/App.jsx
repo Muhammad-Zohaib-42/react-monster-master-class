0618d28ef56d425c2244e67697994cd68d6028df
Muhammad-Zohaib-42 <muhammadzohaibranjha42@gmail.com>
2026-06-02 18:48:11 +0500

    learn about how to use icons and see react icons library and practice it

--- intro/src/App.jsx
+++ intro/src/App.jsx
@@ -1,8 +1,10 @@
+import { FaCartArrowDown } from "react-icons/fa";
 import "./index.css"
 
 const App = () => {
   return (
     <section>
+      <FaCartArrowDown />
       <h1>Hello World</h1>
     </section>
   )
