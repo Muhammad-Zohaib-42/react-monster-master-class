082cc54df28fe9d84bc93591c352cd8f0f3fe13f
Muhammad-Zohaib-42 <muhammadzohaibranjha42@gmail.com>
2026-06-02 16:56:36 +0500

    challenge 3 completed create a welcome message component and show on screen

--- intro/src/components/Footer.jsx
+++ intro/src/components/Footer.jsx
@@ -1,12 +1,11 @@
-import React from "react";
-import { jsx as _jsx } from "react/jsx-runtime";
+import React from 'react'
 
 const Footer = () => {
-  return _jsx("footer", {
-    children: _jsx("p", {
-      children: "\xA9 2026 My Website",
-    }),
-  });
-};
+  return (
+    <footer>
+        <p>&copy; 2026 My Website</p>
+    </footer>
+  )
+}
 
-export default Footer;
+export default Footer
--- intro/src/components/MainContent.jsx
+++ intro/src/components/MainContent.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import WelcomeMessage from './WelcomeMessage'
 
 const MainContent = () => {
   return (
     <main>
+        <WelcomeMessage />
         <h2>Main Content</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam voluptatibus optio modi quibusdam possimus at impedit rerum delectus! Amet, libero cumque. Velit odio, tenetur debitis exercitationem nihil architecto doloremque atque!</p>
     </main>
