64745dde5acc805608aab2eaaca7e028578c43d6
Muhammad-Zohaib-42 <muhammadzohaibranjha42@gmail.com>
2026-06-03 19:13:44 +0500

    learn the useReducer hook and practice it by creating a count feature

--- intro/src/App.jsx
+++ intro/src/App.jsx
@@ -1,16 +1,35 @@
-import React, { useContext } from 'react'
-import { AuthContext } from './contexts/AuthContext.jsx'
-import Header from './components/Header'
-import MainContent from './components/MainContent.jsx'
+import { useReducer, useState } from "react"
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return state + action.payload
+      break
+    case 'decrement':
+      return state - action.payload
+      break
+    case 'reset':
+      return 0
+      break
+    default:
+      return state
+  }
+}
 
 const App = () => {
-  const {isAdmin, setIsAdmin} = useContext(AuthContext)
+  const [inputValue, setInputValue] = useState(1)
+  const [state, dispatch] = useReducer(reducer, 0)
 
   return (
-    <>
-      <Header />
-      <MainContent />
-    </>
+    <section>
+      <div>
+        <input type="number" value={inputValue} onChange={e=>setInputValue(e.target.value)} />
+        <button onClick={() => dispatch({type: 'increment', payload: Number(inputValue)})}>+</button>
+        <button onClick={() => dispatch({type: 'decrement', payload: Number(inputValue)})}>-</button>
+        <button onClick={() => dispatch({type: 'reset'})}>reset</button>
+      </div>
+      <h1>Count: {state}</h1>
+    </section>
   )
 }
 
