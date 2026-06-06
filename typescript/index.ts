29f47c0ed7363b93300123a6845d78ac3cbe5a1b
Muhammad-Zohaib-42 <muhammadzohaibranjha42@gmail.com>
2026-06-07 04:58:22 +0500

    learn about function parameter and type inference

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,3 +1,8 @@
-let myName: any = 'zohaib'
-myName = 23
-myName = true
+function addOne(num: number): number {
+    return num + 1
+}
+
+const result = addOne(1)
+console.log(result)
+
+const square = (x:number):number => x * x
