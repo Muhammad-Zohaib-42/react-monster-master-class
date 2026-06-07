d78987cf4ec9a95d2e6fde04a56d55efd53851ed
Muhammad-Zohaib-42 <muhammadzohaibranjha42@gmail.com>
2026-06-07 19:03:17 +0500

    learn about getters and setters

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,8 +1,19 @@
-enum WeatherCondition {
-    Sunny = "sunny",
-    Cloudy = "cloudy",
-    Rainy = "rainy",
-    Snowy = "snowy"
+class BankAccount {
+    private myAmount: number = 0
+
+    get getAmount(): number {
+        return this.myAmount
+    }
+
+    set setAmount(amount: number) {
+        this.myAmount = amount
+    }
 }
 
-console.log(WeatherCondition.Sunny)
+const myBankAccount = new BankAccount()
+
+console.log(`my amount: ${myBankAccount.getAmount}`)
+
+myBankAccount.setAmount = 1000000
+
+console.log(`my amount: ${myBankAccount.getAmount}`)
