9b6b8b0734c90a3c271ca75f546de8b617dbca67
Muhammad-Zohaib-42 <muhammadzohaibranjha42@gmail.com>
2026-06-07 16:28:39 +0500

    learn about intersection types how we combine two types and use them

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,12 +1,18 @@
-type Obj = {
-    readonly name: string,
-    age?: number
+type Person = {
+    name: string,
+    age: number
 }
 
-const obj: Obj = {
+type Employee = {
+    id: number,
+    title: string
+}
+
+type PersonAndEmployee = Person & Employee
+
+const user: PersonAndEmployee = {
     name: "zohaib",
+    age: 23,
+    id: 2,
+    title: "employee"
 }
-
-obj.name = "shoaib"
-
-console.log(obj)
