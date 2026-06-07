bbe8d34450133c350b8d559bcd5afbae66618694
Muhammad-Zohaib-42 <muhammadzohaibranjha42@gmail.com>
2026-06-07 16:54:25 +0500

    lern about the union types in typescript

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,18 +1,1 @@
-type Person = {
-    name: string,
-    age: number
-}
-
-type Employee = {
-    id: number,
-    title: string
-}
-
-type PersonAndEmployee = Person & Employee
-
-const user: PersonAndEmployee = {
-    name: "zohaib",
-    age: 23,
-    id: 2,
-    title: "employee"
-}
+const items: (number | string)[] = [1,2,'zohaib']
