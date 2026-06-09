e7fc834784c1a90757848e9ba8cf92f6a8f29ded
Muhammad-Zohaib-42 <muhammadzohaibranjha42@gmail.com>
2026-06-10 01:54:01 +0500

    typescript vanilla done now gonna learn with react

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,17 +1,1 @@
-type User = {
-    username: string
-    email: string
-}
-
-type Admin = {
-    dashboard: boolean
-}
-
-type NewUser = User & Admin
-
-const newUser: NewUser = {
-    username: "zohaib",
-    email: "zohaib@gmail.com",
-    dashboard: true
-}
-
+console.log(document)
