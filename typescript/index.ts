6acdf6e843c27558eb248e91ed612281c01b070b
Muhammad-Zohaib-42 <muhammadzohaibranjha42@gmail.com>
2026-06-10 01:23:37 +0500

    learning type narrowing

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,40 +1,12 @@
-class Box<T> {
-    private content: T
+type MyType = string | number
 
-    constructor(content: T) {
-        this.content = content
-    }
-
-    getContent(): T {
-        return this.content
-    }
-    
-    setContent(content: T): void {
-        this.content = content
+function exampleFunction(value: MyType): void {
+    if (typeof value === "string") {
+        console.log(value.toUpperCase())
+    } else {
+        console.log(value.toFixed(2))
     }
 }
 
-const contentBox = new Box<string>("Hello World")
-console.log(contentBox.getContent())
-contentBox.setContent("Hello Ts")
-console.log(contentBox.getContent())
-function filterArray<T>(array: T[], condition: (item:T) => boolean) {
-    return array.filter(item => condition(item))
-}
-
-const array = [1,2,3,4,5,6,7,8,9,10]
-
-const evenNumbers = filterArray<number>(array, (item) => item % 2 == 0)
-
-console.log(evenNumbers)
-
-interface Fruit {
-    name: string
-    color: string
-}
-
-const fruitsArray: Fruit[] = [
-    {name: "apple", color: "red"},
-    {name: "apple", color: "red"},
-    {name: "apple", color: "red"},
-]
+exampleFunction('zohaib')
+exampleFunction(33)
