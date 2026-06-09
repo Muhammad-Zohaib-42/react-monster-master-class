88313445112062a41817913ed04c603559a69f30
Muhammad-Zohaib-42 <muhammadzohaibranjha42@gmail.com>
2026-06-10 01:28:57 +0500

    learn the type narrowing using instance of operator

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,12 +1,25 @@
-type MyType = string | number
-
-function exampleFunction(value: MyType): void {
-    if (typeof value === "string") {
-        console.log(value.toUpperCase())
-    } else {
-        console.log(value.toFixed(2))
+class Dog {
+    bark(): void {
+        console.log("barking...")
     }
 }
 
-exampleFunction('zohaib')
-exampleFunction(33)
+class Cat {
+    meow(): void {
+        console.log("meow...")
+    }
+}
+
+function animalsSound(animal: Dog | Cat):void {
+    if (animal instanceof Dog) {
+        animal.bark()
+    } else {
+        animal.meow()
+    }
+}
+
+const dog = new Dog()
+const cat = new Cat()
+
+animalsSound(dog)
+animalsSound(cat)
