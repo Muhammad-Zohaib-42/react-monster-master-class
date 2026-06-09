7a56fa1b2a25a0f6380e50bc8b4217b695ed326b
Muhammad-Zohaib-42 <muhammadzohaibranjha42@gmail.com>
2026-06-09 10:28:00 +0500

    learning generics in typescript...

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,22 +1,20 @@
-interface Car {
-    brand: string
-    start(): void
-}
-// declaration merging
-interface Car {
-    model: string,
-    stop(): void
+function filterArray<T>(array: T[], condition: (item:T) => boolean) {
+    return array.filter(item => condition(item))
 }
 
-const car: Car = {
-    brand: "BMW",
-    start() {
-        console.log('start')
-    },
-    stop() {
-        console.log('stop')
-    },
-    model: "M3"
+const array = [1,2,3,4,5,6,7,8,9,10]
+
+const evenNumbers = filterArray<number>(array, (item) => item % 2 == 0)
+
+console.log(evenNumbers)
+
+interface Fruit {
+    name: string
+    color: string
 }
 
-car.start()
+const fruitsArray: Fruit[] = [
+    {name: "apple", color: "red"},
+    {name: "apple", color: "red"},
+    {name: "apple", color: "red"},
+]
