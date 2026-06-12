f99b31b105f2cf4d97d513ca89e419e84f963314
Muhammad-Zohaib-42 <muhammadzohaibranjha42@gmail.com>
2026-06-12 11:37:00 +0500

    learn two ways how we handle props in react like FC and other way

--- typescript/src/components/User.tsx
+++ typescript/src/components/User.tsx
@@ -1,7 +1,15 @@
-const User = ({username, email}) => {
+import type { FC } from "react"
+
+interface UserShape {
+  username: string,
+  email: string
+}
+
+const User: FC<UserShape> = ({username, email}) => {
   return (
     <div>
         <p>username: {username}</p>
+        <p>email: {email}</p>
     </div>
   )
 }
