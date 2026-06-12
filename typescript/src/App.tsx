ea22eef411e4d34b17eb3b4be6575f91f829ed11
Muhammad-Zohaib-42 <muhammadzohaibranjha42@gmail.com>
2026-06-12 11:33:38 +0500

    learning typescript with react learn how we handle propers

---
+++ typescript/src/App.tsx
@@ -0,0 +1,11 @@
+import User from "./components/User"
+
+function App() {
+  return (
+    <>
+      <User username="zohaib" email="zohaib@gmail.com" />
+    </>
+  )
+}
+
+export default App
---
+++ typescript/src/components/User.tsx
@@ -0,0 +1,9 @@
+const User = ({username, email}) => {
+  return (
+    <div>
+        <p>username: {username}</p>
+    </div>
+  )
+}
+
+export default User
