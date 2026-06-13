bbf9ed045a87d1fbfd5937c1fad8352c4c5eff17
Muhammad-Zohaib-42 <muhammadzohaibranjha42@gmail.com>
2026-06-13 14:51:43 +0500

    learning how to use ts with useReducer hook

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,9 +1,9 @@
-import Counter from "./components/Counter"
+import CounterReducer from "./components/CounterReducer"
 
 function App() {
   return (
     <>
-      <Counter />
+      <CounterReducer />
     </>
   )
 }
