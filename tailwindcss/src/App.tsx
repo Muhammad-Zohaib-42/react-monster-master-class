16292ffa547d60b7d603b0945470fbaded5bc4d2
Muhammad-Zohaib-42 <muhammadzohaibranjha42@gmail.com>
2026-06-15 18:36:25 +0500

    use transition project done

--- tailwindcss/src/App.tsx
+++ tailwindcss/src/App.tsx
@@ -1,10 +1,12 @@
 import { Suspense } from "react"
 import FetchData from "./components/ui/FetchData"
+import Counter from "./components/Counter.tsx"
 
 const App = () => {
   return (
     <Suspense>
       <FetchData />
+      <Counter />
     </Suspense>
   )
 }
