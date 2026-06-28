a8d05f74cb3e9e88032e466e2018a11f4ec8f819
Muhammad-Zohaib-42 <muhammadzohaibranjha42@gmail.com>
2026-06-28 13:50:45 +0500

    make the categories dynamic

--- react-store/src/components/CategoriesSection.tsx
+++ react-store/src/components/CategoriesSection.tsx
@@ -1,14 +1,20 @@
+import { useContext } from "react"
 import RadioInput from "./RadioInput"
+import { StoreContext } from "../contexts/StoreContext"
 
 const CategorySection = () => {
-  const radioItems: string[] = ['beauty', 'fragrances', 'furniture', 'groceries']
+  const {productsData} = useContext(StoreContext)!
+  const radioItems: string[] = productsData.reduce<string[]>((acc,curr) => {
+    if (!acc.find(elem => elem == curr.category)) acc.push(curr.category)
+    return acc
+  }, [])
 
   return (
     <div className="my-5">
         <h2 className="section-title mb-0.5">Categories</h2>
         <ul className="section-list">
           {
-              radioItems.map(radioItem => <RadioInput key={radioItem} text={radioItem} />)
+              productsData.length < 1 ? <p>Loading Categories...</p> : radioItems.map(radioItem => <RadioInput key={radioItem} text={radioItem} />)
           }
         </ul>
     </div>
