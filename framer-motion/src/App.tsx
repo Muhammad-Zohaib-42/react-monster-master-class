137ebf2656c1c6325faeeb5d7c3666ca8544380e
Muhammad-Zohaib-42 <muhammadzohaibranjha42@gmail.com>
2026-07-01 17:18:39 +0500

    creates a loading boucing ball animation using motion

--- framer-motion/src/App.tsx
+++ framer-motion/src/App.tsx
@@ -1,8 +1,26 @@
+import {motion} from "motion/react"
+
 function App() {
   return (
-    <>
-      <h1 className="bg-red-700 text-white">Hello World</h1>
-    </>
+    <main className="h-screen bg-slate-900 text-slate-200 grid place-content-center">
+      <div className="flex items-center gap-2">
+        {
+          [...Array(3)].map((_,index) => <motion.div 
+            className="w-10 h-10 bg-teal-300 rounded-full"
+            key={index}
+            animate= {{
+              y: [0, -15, 0]
+            }}
+            transition={{
+              duration: 0.6,
+              repeat: Infinity,
+              ease: "easeInOut",
+              repeatDelay: index * 0.2
+            }}
+          />)
+        }
+      </div>
+    </main>
   )
 }
 
