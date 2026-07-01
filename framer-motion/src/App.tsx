bef8f2d3a9b71a1b443b82097805e8a5cfa362fa
Muhammad-Zohaib-42 <muhammadzohaibranjha42@gmail.com>
2026-07-01 17:59:53 +0500

    create flip card animation using motion

--- framer-motion/src/App.tsx
+++ framer-motion/src/App.tsx
@@ -1,25 +1,20 @@
 import {motion} from "motion/react"
+import { useState } from "react"
+
+const variants = {
+  front: {rotateY: 0},
+  back: {rotateY: 180}
+}
 
 function App() {
+  const [isFlipped, setIsFlipped] = useState<boolean>(false)
+
   return (
-    <main className="h-screen bg-slate-900 text-slate-200 grid place-content-center">
-      <div className="flex items-center gap-2">
-        {
-          [...Array(3)].map((_,index) => <motion.div 
-            className="w-10 h-10 bg-teal-300 rounded-full"
-            key={index}
-            animate= {{
-              y: [0, -15, 0]
-            }}
-            transition={{
-              duration: 0.6,
-              repeat: Infinity,
-              ease: "easeInOut",
-              repeatDelay: index * 0.2
-            }}
-          />)
-        }
-      </div>
+    <main onClick={() => setIsFlipped(!isFlipped)} className="h-screen bg-slate-900 text-slate-200 grid place-content-center perspective-distant">
+      <motion.div variants={variants} initial="front" animate={isFlipped ? "back" : "front"} transition={{duration:1, ease: "anticipate"}} className="relative h-70 w-60 transform-3d">
+        <div className="absolute inset-0 text-xl flex items-center justify-center backface-hidden bg-slate-800 rounded-md">Front Side</div>
+        <div className="absolute inset-0 text-xl flex items-center justify-center backface-hidden bg-slate-800 rounded-md rotate-y-180">Back Side</div>
+      </motion.div>
     </main>
   )
 }
